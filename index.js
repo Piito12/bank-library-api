9f8b286cf1419f56fd11fb9e8cd3630b62c39158
sitthipong <sitthipong18748@gmail.com>
2025-02-05 19:02:37 +0700

    UPDATE : update code

--- index.js
+++ index.js
@@ -33,20 +33,6 @@ app.post("/login", (req, res) => {
     res.status(401).json({ error: "Invalid credentials" });
 });
 
-
-// // Login Endpoint
-// app.post('/login', async (req, res) => {
-//     const { username, password } = req.body;
-//     const user = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
-    
-//     if (user.rows.length === 0 || !(await bcrypt.compare(password, user.rows[0].password))) {
-//         return res.status(401).json({ message: 'Invalid Credentials' });
-//     }
-    
-//     const token = jwt.sign({ id: user.rows[0].id, username }, JWT_SECRET, { expiresIn: '1h' });
-//     res.json({ token });
-// });
-
 // Add a Book
 app.post('/books', verifyToken, async (req, res) => {
     const { title, author, isbn, published_year } = req.body;
